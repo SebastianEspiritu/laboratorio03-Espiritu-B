59d2d520ce52174ecb08eb31b4fb63d08fadb7b4
Sebastian Espiritu <sebastian.espiritu@tecsup.edu.pe>
2026-09-05 14:07:42 -0500

    feat: implementacion de ProductoRepository y ProductoService

---
+++ src/main/java/com/tecsup/demo01/service/ProductoService.java
@@ -0,0 +1,29 @@
+package com.tecsup.demo01.service;
+
+import com.tecsup.demo01.model.Producto;
+import com.tecsup.demo01.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductoService {
+    @Autowired
+    private ProductoRepository repo;
+
+    public List<Producto> listar() {
+        return repo.findAll();
+    }
+
+    public Producto guardar(Producto p) {
+        return repo.save(p);
+    }
+
+    public Producto obtener(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public void eliminar(Long id) {
+        repo.deleteById(id);
+    }
+}
